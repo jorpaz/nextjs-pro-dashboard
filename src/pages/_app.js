bbde44e25c8480094b46a8029fadbaee7df9a080
jorpaz <giovanny.paz@outlook.com>
2023-03-20 15:52:35 -0600

    layout & components integration

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
+import MainLayout from '@Layout/MainLayout';
 import '@styles/tailwind.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </>
+  );
 }
